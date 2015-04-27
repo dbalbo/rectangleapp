d4c4b44d01619921a13427b71dc8d3999590d6b1
dbalbo <debbalbo@Hotmail.com>
2015-04-26 22:40:57 -0700

    just want access to homework

---
+++ views/layout.rb
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rectangle</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap">
+</head>
+<body>
+	<div class="container">
+		<%= yield %>
+		</div>
+</body>
+</html>		
+
